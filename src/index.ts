2f5e8a320cb3fe956ee55e806abac3252e1edd36
GulSam00 <gulsamcono@gmail.com>
2024-10-28 13:40:03 +0900

    fix : module NodeNext로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import { getKakaoLocal, getKakaoNcst } from './getExactNcst';
+import { getKakaoLocal, getKakaoNcst } from './getExactNcst.js';
+// nodenext 사용 시 확장자를 생략하면 안됨
 
 export { getKakaoLocal, getKakaoNcst };
